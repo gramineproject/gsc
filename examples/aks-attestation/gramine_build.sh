06a63d75e5b342d8377ada413e9f596b23368223
Veena Saini <veena.saini@intel.com>
2021-12-03 16:15:39 +0530

    fixup! added base server and client image generation script
    Signed-off-by: Veena Saini <veena.saini@intel.com>

--- examples/aks-attestation/gramine_build.sh
+++ examples/aks-attestation/gramine_build.sh
@@ -31,6 +31,13 @@ mkdir -p meson_build_output
 # Generate Signing Key
 openssl genrsa -3 -out Pal/src/host/Linux-SGX/signer/enclave-key.pem 3072
 
+# Install DCAP dependencies
+echo 'deb [arch=amd64] https://download.01.org/intel-sgx/sgx_repo/ubuntu bionic main' | \
+    sudo tee /etc/apt/sources.list.d/intel-sgx.list
+wget https://download.01.org/intel-sgx/sgx_repo/ubuntu/intel-sgx-deb.key
+sudo apt-key add intel-sgx-deb.key
+sudo apt-get install -y libsgx-dcap-quote-verify-dev
+
 # Build Gramine with DCAP enabled mode (assuming in-kernel driver)
 meson setup build/ --prefix="$PWD/meson_build_output" --buildtype=release -Ddirect=enabled \
   -Dsgx=enabled -Ddcap=enabled
