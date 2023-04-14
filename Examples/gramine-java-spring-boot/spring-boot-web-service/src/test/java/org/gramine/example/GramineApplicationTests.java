4f80bac06134b7222ce8effaefa22e0487dc330e
b-garbacz <bartlomiej.garbacz@intel.com>
2023-04-14 10:45:48 +0200

    Add Java examples
    Signed-off-by: b-garbacz <bartlomiej.garbacz@intel.com>

---
+++ Examples/gramine-java-spring-boot/spring-boot-web-service/src/test/java/org/gramine/example/GramineApplicationTests.java
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+// Copyright (C) 2023 Intel Corporation
+package org.gramine.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GramineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
