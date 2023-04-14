309f4e545846edfd255b173937acff7a06ea7f45
b-garbacz <bartlomiej.garbacz@intel.com>
2023-04-14 10:45:48 +0200

    Add Java examples
    Signed-off-by: b-garbacz <bartlomiej.garbacz@intel.com>

---
+++ Examples/gramine-java-spring-boot/spring-boot-web-service/src/main/java/org/gramine/example/GramineApplication.java
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+// Copyright (C) 2023 Intel Corporation
+package org.gramine.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GramineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GramineApplication.class, args);
+    }
+
+}
