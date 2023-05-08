73412e70c503f28057bf799e475f929e28387440
b-garbacz <bartlomiej.garbacz@intel.com>
2023-05-08 15:47:39 +0200

    Corrected class name
    Signed-off-by: b-garbacz <bartlomiej.garbacz@intel.com>

--- Examples/java-simple/JavaExample.java
+++ Examples/java-simple/JavaExample.java
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 // Copyright (C) 2023 Intel Corporation
-class GramineJavaExample {
+class JavaExample {
     public static void main(String[] args) {
         System.out.println("Hello from Gramine!");
     }
