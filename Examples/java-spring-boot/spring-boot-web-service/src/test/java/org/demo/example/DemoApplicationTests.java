503fbf103b109d76da6a2641f24bb48e65a0ccb1
b-garbacz <bartlomiej.garbacz@intel.com>
2023-05-19 07:48:06 +0200

    Replaced prefix Gramine with Demo.
    Signed-off-by: b-garbacz <bartlomiej.garbacz@intel.com>

--- Examples/java-spring-boot/spring-boot-web-service/src/test/java/org/demo/example/DemoApplicationTests.java
+++ Examples/java-spring-boot/spring-boot-web-service/src/test/java/org/demo/example/DemoApplicationTests.java
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 // Copyright (C) 2023 Intel Corporation
-package org.gramine.example;
+package org.demo.example;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GramineApplicationTests {
+class DemoApplicationTests {
 
     @Test
     void contextLoads() {
