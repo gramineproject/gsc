8d972f3a5431bcec716835ffc0f742a18bb45479
b-garbacz <bartlomiej.garbacz@intel.com>
2024-03-14 01:22:31 -0700

    [Examples] Add two Java examples
    First example is a trivial Java application. Second example is a Spring
    Boot web application.
    
    Signed-off-by: b-garbacz <bartlomiej.garbacz@intel.com>

---
+++ Examples/java-spring-boot/spring-boot-web-service/src/main/java/org/demo/example/DemoApplication.java
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (C) 2023 Intel Corp.
+//                    Bartłomiej Garbacz <bartomiej.garbacz@intel.com>
+package org.demo.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
