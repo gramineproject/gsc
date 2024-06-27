92e8037bf510bb95e2db0c48bd3e00f0399da70e
adarshan-intel <adarsh.anand@intel.com>
2024-06-27 10:11:20 +0000

    Add version_id for CentOS Stream 9
    Signed-off-by: adarshan-intel <adarsh.anand@intel.com>

--- gsc.py
+++ gsc.py
@@ -260,7 +260,7 @@ def get_image_distro(docker_socket, image_name):
         except docker.errors.ContainerError:
             distro = 'redhat/ubi8:' + version_id
 
-    if (os_release['ID'] == 'centos'):
+    if (os_release['ID'] == 'centos' and version_id=='9'):
         distro = 'quay.io/centos/centos:stream' + version_id
 
     return distro
