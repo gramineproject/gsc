4ff96165efa1a45cdb7322fda0af03f076aa87bf
adarshan-intel <adarsh.anand@intel.com>
2024-08-14 12:27:52 +0530

    fixup! Add support for SLES 15.4 and openSUSE Leap 15.6
    Signed-off-by: adarshan-intel <adarsh.anand@intel.com>

--- gsc.py
+++ gsc.py
@@ -116,9 +116,10 @@ def extract_binary_info_from_image_config(config, env):
 
 def extract_environment_from_image_config(config):
     env_list = config['Env']
-    base_image_environment = ''
     if env_list is None:
         return ''
+    
+    base_image_environment = ''
     for env_var in env_list:
         # TODO: switch to loader.env_src_file = "file:file_with_serialized_envs" if
         # the need for multi-line envvars arises
